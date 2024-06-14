2038a491c9f9c0b052786dbbff1aac05da005663
Ariel Cabeza <cervanteslibch@gmail.com>
2024-06-14 20:16:59 -0300

    enviando mensajes

--- server/index.js
+++ server/index.js
@@ -7,6 +7,13 @@ const app = express();
 const server = http.createServer(app);
 const io = new SocketServer(server);
 
+io.on('connection', socket => {
+    console.log("Cliente conectado");
+
+    socket.on('message', (data) => {
+        console.log(data)
+    })
+})
 
 server.listen(3000, () => {
     console.log("Listening on port 3000");
